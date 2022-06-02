6e1673c5395b0a7d362431d736321b79d5c82671
Valeria Piont <iceflame.vvv@gmail.com>
2022-06-02 19:12:34 +0300

    udated information about bot

--- src/main/java/com/karazin/diary_bot/bot/util/DefaultBotMessage.java
+++ src/main/java/com/karazin/diary_bot/bot/util/DefaultBotMessage.java
@@ -8,7 +8,11 @@ public enum DefaultBotMessage {
     ENTER_NEW_POST("Enter your text of new post"),
     CHOOSE_POST("Choose post"),
     EMPTY_LIST("You haven’t added posts yet"),
-    ABOUT("${telegram.about}"),
+    ABOUT("Hi! I'm note bot. Available actions:\n* Add new note\n" +
+            "* Update note (all or part)\n* Delete note\n" +
+            "* Update your note title (by default title is creation time)\n" +
+            "* Get all notes\n" +
+            "Use keyboard to work with bot."),
     DELETED("The post was deleted successfully"),
     ENTER_NEW_TITLE("Enter post title"),
     ENTER_NEW_TEXT_POST("Enter new text of post"),
@@ -17,7 +21,8 @@ public enum DefaultBotMessage {
     UPDATED("Successfully updated"),
     ADDED("Successfully added"),
     UNKNOWN_TEXT("I don't understand you :(. Try to use /show_all_posts command"),
-    INVALID_INPUT("You should send me the text");
+    INVALID_TEXT("You should send me the text"),
+    INVALID_TITLE("Title should be less then 50 characters");
 
     private String message;
 
